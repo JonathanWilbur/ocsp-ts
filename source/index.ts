a5419a364cc0ab25e617dde2ed7aca7490189491
Jonathan M. Wilbur <jonathan@wilbur.space>
2021-01-12 20:06:24 -0500

    v1.0.0
    Signed-off-by: Jonathan M. Wilbur <jonathan@wilbur.space>

--- source/index.ts
+++
@@ -1,1 +0,0 @@
-export * as OCSP_2013_08 from "./OCSP-2013-08";
---
+++ source/modules/OCSP-2013-08/id-pkix-ocsp.va.ts
@@ -0,0 +1,21 @@
+/* eslint-disable */
+import { OBJECT_IDENTIFIER } from "asn1-ts";
+import { id_ad_ocsp } from "x500-ts/dist/node/modules/PkiPmiExternalDataTypes/id-ad-ocsp.va";
+
+/* START_OF_SYMBOL_DEFINITION id_pkix_ocsp */
+/**
+ * @summary id_pkix_ocsp
+ * @description
+ *
+ * ### ASN.1 Definition:
+ *
+ * ```asn1
+ * id-pkix-ocsp                 OBJECT IDENTIFIER ::= id-ad-ocsp
+ * ```
+ *
+ * @constant
+ */
+export const id_pkix_ocsp: OBJECT_IDENTIFIER = id_ad_ocsp;
+/* END_OF_SYMBOL_DEFINITION id_pkix_ocsp */
+
+/* eslint-enable */
